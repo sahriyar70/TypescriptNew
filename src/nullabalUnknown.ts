0c6d0252e2e367746683c9346351ac118c48cd8b
Al Sahriyar <alsahriyar229@gmail.com>
2025-11-10 12:05:06 +0600

    nullabalUnknown of ts

---
+++ src/nullabalUnknown.ts
@@ -0,0 +1,22 @@
+ 
+ // nullabalUnknown.ts
+
+ const getuser = (input : string | null) => {
+    if(input){
+        console.log(`user name is ${input}`);   
+    } else {
+        console.log("no user found");
+    }  }
+
+    getuser(null);
+    getuser("sahriyar");
+
+    const getuser2 = (input : unknown) => {
+        if(typeof input === "string"){
+            console.log(`user name is ${input}`);   
+        } else {
+            console.log("no user found");
+        }  }
+
+    getuser2(null);
+    getuser2("sahriyar");
