c46b7bd0403a5985e9b191a9c01d9077d6a135f8
Al Sahriyar <alsahriyar229@gmail.com>
2025-11-10 06:43:16 +0600

    union of ts

---
+++ src/union.ts
@@ -0,0 +1,17 @@
+// union of ts
+
+type user = "addmin" | "user" | "super-admin"; // union type alias
+
+const getDesboard = (role : user) => {
+    if(role === "addmin"){
+
+        console.log("You have full access"); } 
+    
+    else if(role === "super-admin"){
+
+        console.log("You have super access");  } }
+
+getDesboard("addmin");
+getDesboard("super-admin");
+getDesboard("gust");
+// getDesboard("guest"); // error because guest is not in union typep
